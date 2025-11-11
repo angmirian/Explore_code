6a2ae8514e017bf1d392734fee2fd406ea565bbf
Angela Ianni <angmirian@users.noreply.github.com>
2025-11-11 15:51:18 -0500

    Add files via upload

--- Plot_mixed_by.R
+++ Plot_mixed_by.R
@@ -1,5 +1,6 @@
 #AI - Plot_mixed_byI.R; uses ddf generated from Build_mixed_by_modular.R
-#Last updated May 1, 2024
+#Need to change name of data_subfolder each time you run this
+#Updated 5/2/2025 to make lines thicker
 
 rm(list=ls())
 
@@ -9,26 +10,28 @@ library(ggplot2)
 library(tidyverse)
 library(viridis)
 library(grid)
-library(viridis) 
+library(viridis)
 
 ########## User Settings ##########
 align_to = "clock" #options are clock or response 
 structure = "network" 
 toplot <- "estimate" #options: are estimate and statistic
 region_to_run = "cortical" #options are cortical (vmPFC) and subcortical
-subjs_to_run = "all" #options are all or controls_only
-flip_stat = 1 #to flip statistic for plots (i.e. if model set up to look at attempters vs. depressed but you want to flip to see depressed vs. attempters)
+subjs_to_run = "patients_only" #options are all, controls_only, and patients_only
+flip_stat = 0 #to flip statistic for plots (i.e. if model set up to look at attempters vs. depressed but you want to flip to see depressed vs. attempters)
 if (align_to=="clock") {
   exclude_prefeedback = 0 #set to 1 if you want to exclude the pre-feedback results from plots
 } else if (align_to=="response") {exclude_prefeedback = 1}
 select_networks=TRUE #can only set to TRUE for subcortical; if true only do Cont, Default, Limbic, hipp, and amygdala
 
-subcortical_cache_dir = '/Users/angela/Documents/Research/Explore/Medusa_analysis/subcortical' 
-cortical_cache_dir = '/Users/angela/Documents/Research/Explore/Medusa_analysis/cortical'
+subcortical_cache_dir = '/Users/angela/Documents/Work/Explore/Medusa_analysis/subcortical' 
+cortical_cache_dir = '/Users/angela/Documents/Work/Explore/Medusa_analysis/cortical'
 
-if (region_to_run=="cortical") {data_subfolder <- "Patients_only"
-} else {data_subfolder <- "Selected_subcortical_regions/Patients_only" }
-
+if (region_to_run=="cortical") {
+  data_subfolder <- "Impulsivity_updatedNov2025/Patients_only" #"All_participants"
+} else if(region_to_run=="subcortical") {
+  data_subfolder <- "Selected_subcortical_regions/Impulsivity_updatedNov2025/Patients_only" #Extraversion/Patients_only 
+  }
 
 
 ########## Load data ##########
@@ -70,7 +73,7 @@ for (i in 1:length(data_files$V1)) {
   } else if(align_to == "clock"){epoch_label <- 'clock'}
   #Select what you want to plot
   if (toplot == "statistic") {ylabel <- 't-statistic'
-  } else if(toplot == "estimate") {ylabel <- 'Coefficient (AU)'}
+  } else if(toplot == "estimate") {ylabel <- 'Coefficient (a.u.)'}
   
   ########## Get data ready to plot ##########
   # ddf is the output structure; reml and ml are two different convergence methods (use reml - contains AIC and BIC andn log likelihood)
@@ -93,14 +96,44 @@ for (i in 1:length(data_files$V1)) {
   ddf$t <- as.numeric(ddf$t)
 
   #define color schemes
-  color_options <- c('#66CCEE','#c912ab','#e17014','#32af64','#644bc8','blue','firebrick1','darkseagreen1','gold')
+  #color_options <- c('#66CCEE','#c912ab','#e17014','#32af64','#644bc8','blue','firebrick1','darkseagreen1','gold')
+  #color_options <- c('#53a6c2','#c912ab','#e17014','#32af64','#644bc8','blue','firebrick1','darkseagreen1','gold') #for option 1 with lighter background
+  color_options <- c('#53a1c2','#c912ab','#e17014','#32af64','#644bc8','blue','firebrick1','darkseagreen1','gold') #for option 2 with lighter background
+  
+  
   if (region_to_run == "subcortical") {
     color_scheme <- c(color_options[1:length(unique(ddf$network))])
-    background_color='gray80'
-    major_grid_color='gray90' 
-    minor_grid_color='gray80' 
-    hline_color='white' 
-    vline_color='white'
+
+    #Changed May 2, 2025 to address Michael's comment
+    ##original options
+    #background_color='gray80'
+    #major_grid_color='gray90' 
+    #minor_grid_color='gray80' 
+    #hline_color='white' 
+    #vline_color='white'
+    
+    ##option with darker background
+    #background_color='gray70'
+    #major_grid_color='gray90' 
+    #minor_grid_color='gray70' 
+    #hline_color='white' 
+    #vline_color='white'
+    
+    ###Option 1 with lighter background
+    #background_color='gray95'
+    #major_grid_color='gray80' 
+    #minor_grid_color='gray95' 
+    #hline_color='gray40' 
+    #vline_color='gray40'
+    
+    ###Option 2 with lighter background
+    background_color='gray90'
+    major_grid_color='gray80' 
+    minor_grid_color='gray90' 
+    hline_color='gray40' 
+    vline_color='gray40'
+    
+    
   } else if (region_to_run == "cortical") {
     color_scheme <- c('#c912ab','#e17014','#644bc8')
     background_color='white' 
@@ -135,32 +168,37 @@ for (i in 1:length(data_files$V1)) {
     termstr <- str_replace_all(fe, "[^[:alnum:]]", "_")
     if (region_to_run == "subcortical") {
       if (flip_stat) {fname = paste0(fe,'-Plot-',structure,'-',model_name,"-FLIPPED.pdf")}
-      else {fname = paste0(align_to,'-',fe,'-Plot-',structure,'-',model_name,".pdf")}
+      else {fname = paste0(align_to,'-',fe,'-Plot-',structure,'-',model_name,"_linesize3.pdf")}
     } else if (region_to_run == "cortical") {
       if (flip_stat) {fname = paste0('vmPFC-',align_to,'-',fe,'-Plot-',structure,'-',model_name,"-FLIPPED.pdf")}
-      else {fname = paste0('vmPFC-',align_to,'-',fe,'-Plot-',structure,'-',model_name,".pdf")}}
+      else {fname = paste0('vmPFC-',align_to,'-',fe,'-Plot-',structure,'-',model_name,"_linesize3.pdf")}}
     if (exclude_prefeedback) {
       if (region_to_run == "cortical") {pdf(fname, width = 4.5, height = 4)
       } else {pdf(fname, width = 5, height = 4)}
     } else {pdf(fname, width = 6, height = 4)}
-    pd <- position_dodge(0.2)
+    pd <- position_dodge(0.1) #0.2
     gg<-ggplot(edf, aes(x=t, y=.data[[toplot]], color=network, group=network)) + #changed to flip y for groups compared to attempters 5/16 x=t, y=statistic, color=network)) + 
-      geom_vline(xintercept = 0, lty = 'solid', color = vline_color, size = 1.5)+ xlab(epoch_label) + ylab('') +
-      geom_hline(yintercept = 0, lty = 'solid',color = hline_color, size=1.5) + 
+      geom_vline(xintercept = 0, lty = 'solid', color = vline_color, size = 1.5)+ xlab(epoch_label) + ylab('') + 
+      geom_hline(yintercept = 0, lty = 'solid',color = hline_color, size=1.5) +  
       geom_point(aes(size=`p, FDR-corrected`, alpha=`p, FDR-corrected`), position=pd) + #show.legend = FALSE
       scale_colour_manual(values = color_scheme) + 
-      geom_line(aes(group=network, alpha = `p, FDR-corrected`), size=1.5, position=pd, show.legend = FALSE) + 
+      geom_line(aes(group=network, alpha = `p, FDR-corrected`), size=3, position=pd, show.legend = FALSE) + #line size was 1.5
       geom_linerange(aes(ymin=estimate-std.error, ymax=estimate+std.error, alpha = `p, FDR-corrected`),show.legend = FALSE, position=pd) +
       scale_alpha_discrete(range=c(0.4, 1), drop=FALSE) + scale_size_discrete(range=c(2,6), drop=FALSE) # I think adding this last bit fixed my issue of the points getting resized when all factors weren't in the data
+    if (align_to == "response") {gg <- gg + scale_x_continuous(breaks = seq(0, 2, by = 1))} 
     gg <- gg  + theme_bw(base_size=13) +
-      xlab(paste("time with respect to",align_to," (seconds)")) +
-      ylab(ylabel) + 
+      #xlab(paste("Time with respect to\n",align_to,"(seconds)")) + #commented out 5/2/2025
+      #ylab(ylabel) + # commented out 5/2/2025
       theme(legend.title = element_blank(),
+            axis.title.x = element_blank(), # added 5/2/2025
+            axis.title.y = element_blank(), # added 5/2/2025
             panel.grid.major = element_line(colour = major_grid_color), 
             panel.grid.minor = element_line(colour = minor_grid_color), 
             panel.background = element_rect(fill = background_color),
-            axis.title.y = element_text(margin=margin(r=6)),
-            axis.title.x = element_text(margin=margin(t=6)))
+            #axis.title.y = element_text(size=25, margin=margin(r=6)), # added size 5/2/25
+            #axis.title.x = element_text(size=25, margin=margin(t=6)), # added size 5/2/25
+            axis.text.y = element_text(size=20), #added 5/2/25
+            axis.text.x = element_text(size=20)) #added 5/2/25
     print(gg)
     dev.off()
   }
